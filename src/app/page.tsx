f82a8b06ef44b6b0f55d4bce4c6cab636dd10e8d
raichiqzin <raichiqzin@gmail.com>
2024-02-11 06:52:04 +0000

    dev

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -101,44 +101,46 @@ export default function Home() {
             key={v.id}
             onClick={() => router.push(`/shop/${v.id}`)}
           >
-            <div className="flex flex-col justify-between p-4 leading-normal">
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
-                {v.shopName}
-              </h5>
-              <div className="mb-5 flex items-center">
-                <p className="m-2 inline-flex items-center rounded bg-blue-100 p-1.5 text-3xl font-semibold text-blue-800 ">
-                  {calculateRating(v, "totalRating")}
-                </p>
-                <p className="ms-2 text-2xl font-medium text-gray-900">
-                  総合評価
-                </p>
-                <span className="mx-2 h-1 w-1 rounded-full bg-gray-900"></span>
-                <p className="text-lg font-medium text-gray-500 ">
-                  {v.anke.length ?? 0}件の口コミ
+            <div className="flex flex-col items-center rounded-lg border border-gray-200 bg-white shadow hover:bg-gray-100 md:max-w-xl md:flex-row ">
+              <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">
+                  {v.shopName}
+                </h5>
+                <div className="mb-5 flex items-center">
+                  <p className="m-2 inline-flex items-center rounded bg-blue-100 p-1.5 text-3xl font-semibold text-blue-800 ">
+                    {calculateRating(v, "totalRating")}
+                  </p>
+                  <p className="ms-2 text-2xl font-medium text-gray-900">
+                    総合評価
+                  </p>
+                  <span className="mx-2 h-1 w-1 rounded-full bg-gray-900"></span>
+                  <p className="text-lg font-medium text-gray-500 ">
+                    {v.anke.length ?? 0}件の口コミ
+                  </p>
+                </div>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  <table className="w-full table-auto">
+                    <tbody>
+                      <tr>
+                        <td className="border border-gray-500 px-4 py-2 font-bold">
+                          都道府県
+                        </td>
+                        <td className="border border-gray-500 px-4 py-2">
+                          {v?.prefecture}
+                        </td>
+                      </tr>
+                      <tr>
+                        <td className="border border-gray-500 px-4 py-2 font-bold">
+                          業種
+                        </td>
+                        <td className="border border-gray-500 px-4 py-2">
+                          {v?.serviceType}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
                 </p>
               </div>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                <table className="w-full table-auto">
-                  <tbody>
-                    <tr>
-                      <td className="border border-gray-500 px-4 py-2 font-bold">
-                        都道府県
-                      </td>
-                      <td className="border border-gray-500 px-4 py-2">
-                        {v?.prefecture}
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className="border border-gray-500 px-4 py-2 font-bold">
-                        業種
-                      </td>
-                      <td className="border border-gray-500 px-4 py-2">
-                        {v?.serviceType}
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </p>
             </div>
           </div>
         ))}
